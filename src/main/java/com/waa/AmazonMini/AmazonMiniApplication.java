59b0d3cce3f99d6f36edc29fce2c4481ce71638c
Kapil Dangol <kadangol@miu.edu>
2021-12-10 10:52:07 -0600

    initial project setup

---
+++ src/main/java/com/waa/AmazonMini/AmazonMiniApplication.java
@@ -0,0 +1,13 @@
+package com.waa.AmazonMini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonMiniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonMiniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waa/AmazonMini/AmazonMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waa.AmazonMini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonMiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
