8439ba923d2e02799d2bf784fe5ba9c032442737
alievkomiljon <aliyev.komiljon@gmail.com>
2021-12-14 15:16:33 -0600

    swagger modified

--- src/main/java/com/waa/AmazonMini/configuration/Swagger2Config.java
+++ src/main/java/com/waa/AmazonMini/configuration/Swagger2Config.java
@@ -10,15 +10,16 @@ import org.springframework.web.client.RestTemplate;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -32,12 +33,13 @@ public class Swagger2Config {
     public Docket api() {
 
         Docket docket = new Docket(DocumentationType.SWAGGER_2).apiInfo(apiEndPointsInfo()).pathMapping("/")
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .forCodeGeneration(true).genericModelSubstitutes(ResponseEntity.class)
                 .ignoredParameterTypes(Pageable.class).ignoredParameterTypes(java.sql.Date.class)
                 .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
                 .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
-                .securitySchemes(Arrays.asList(apiKey()))
                 .useDefaultResponseMessages(false);
 
         docket = docket.select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
@@ -52,8 +54,20 @@ public class Swagger2Config {
                 .license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
                 .build();
     }
+
     private ApiKey apiKey() {
-        return new ApiKey("jwtToken", "Authorization", "header");
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 
 }
