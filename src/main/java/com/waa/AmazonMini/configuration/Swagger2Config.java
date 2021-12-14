204078610e765e1a57c20ebc279cd4dde82f3714
alievkomiljon <aliyev.komiljon@gmail.com>
2021-12-14 14:56:11 -0600

    Swagger Auth has been added

--- src/main/java/com/waa/AmazonMini/configuration/Swagger2Config.java
+++ src/main/java/com/waa/AmazonMini/configuration/Swagger2Config.java
@@ -11,11 +11,13 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Date;
 
 @Configuration
@@ -35,6 +37,7 @@ public class Swagger2Config {
                 .directModelSubstitute(java.time.LocalDate.class, java.sql.Date.class)
                 .directModelSubstitute(java.time.ZonedDateTime.class, Date.class)
                 .directModelSubstitute(java.time.LocalDateTime.class, Date.class)
+                .securitySchemes(Arrays.asList(apiKey()))
                 .useDefaultResponseMessages(false);
 
         docket = docket.select().apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
@@ -49,6 +52,8 @@ public class Swagger2Config {
                 .license("Apache 2.0").licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html").version("1.0.0")
                 .build();
     }
-
+    private ApiKey apiKey() {
+        return new ApiKey("jwtToken", "Authorization", "header");
+    }
 
 }
